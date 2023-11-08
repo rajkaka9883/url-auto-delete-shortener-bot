727cb4ee67ee9b879e6a425534dfcfa649adc81d
rajkaka9883 <139911877+rajkaka9883@users.noreply.github.com>
2023-11-08 12:38:50 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -247,7 +247,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://youtube.com/@GreyMattersYT') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('🔻 ᴡᴀᴛᴄʜ & ᴅᴏᴡɴʟᴏᴀᴅ 🔻', callback_data=f'generate_stream_link:{file_id}') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
